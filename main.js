191f7ed753e20b0efe530be8a24216f8403c71c8
Kosuke SHIRAKASHI <k.shira86@gmail.com>
2020-12-21 02:28:47 +0900

    Improve sentEvents managing

--- main.js
+++ main.js
@@ -2,29 +2,58 @@ const NOTIFY_BEFORE_MINUTES = 5 // n分前にLINEに通知
 const NOTIFICATION_HEADER = "🔥次の予定が近づいています🔥"
 
 const SENT_EVENTS = "sentEvents"
+let sentEvents = []
 
 /**
  * 通知送信済みのEventIDを削除する
  * 送信済みEventIDはPropertyに保存してあるため長期間は保持できない。(9kbyte制限)
- * mainTask()内で、終了したEventIDは削除しているが消し忘れがあったときのための保険として実行
+ * mainTask()内で、不要なEventIDは削除しているが消し忘れがあったときのための保険として実行
  * 
- * このタスクが行われる直後に予定が入っていると2回通知が飛ぶことがあるので、予定が入らない深夜帯に設定する。
+ * このタスクが行われる直後に予定が入っていると2回通知が飛ぶことがあるので、予定が入らない時間帯に設定する。
  */
 function dailyTask(){
   deleteProperty(SENT_EVENTS)
 }
 
 function mainTask() {
+  restoreSentEvents()
   let calendar = CalendarApp.getCalendarById(getProperty("CALENDAR_ID"))
   sendNotification(calendar)
   deleteFinishedEvents(calendar)
+  saveSentEvents()
+}
+
+/**
+ * プロパティへのアクセス回数には制限があるため、トリガーの最初に実行する
+ */
+function restoreSentEvents(){
+  sentEvents = JSON.parse(getProperty(SENT_EVENTS))
+  if(!sentEvents){
+    sentEvents = []
+  }
+}
+
+/**
+ * プロパティへのアクセス回数には制限があるため、トリガーの最後に実行する
+ */
+function saveSentEvents(){
+  setProperty(SENT_EVENTS, JSON.stringify(sentEvents))
+}
+
+function removeFromSentEvents(id){
+  const index = sentEvents.indexOf(id)
+  if(index != -1){
+    sentEvents.splice(index, 1)
+    Logger.log(`${id} is removed from sentEvents`)
+  }
 }
 
 /**
  * @param {CalendarApp.Calendar} calendar
  */
 function sendNotification(calendar){
-  let events = calendar.getEvents(new Date(), getRelativeDate(1, 1))
+  let now = new Date()
+  let events = calendar.getEvents(new Date(), minutesLater(now, NOTIFY_BEFORE_MINUTES))
   let msg = events.filter(isNearEvent).map(formatEvent).join("\n\n")
   if(msg){
     Logger.log(`msg => ${msg}`)
@@ -38,25 +67,14 @@ function sendNotification(calendar){
  * @param {CalendarApp.Calendar} calendar
  */
 function deleteFinishedEvents(calendar){
-  let sentEvents = JSON.parse(getProperty(SENT_EVENTS))
   if(!sentEvents || !sentEvents.length) return
-
   let now = new Date()
   let events = calendar.getEvents(getRelativeDate(0, 0), now)
-  let deleted = false
   for(const event of events){
-      let id = event.getId()
-      let end = new Date(event.getEndTime())
-      if(end > now) continue
-      const index = sentEvents.indexOf(id)
-      if(index != -1){
-        sentEvents.splice(index, 1)
-        Logger.log(`${id} is removed from sentEvents`)
-        deleted = true
-      }
-  }
-  if(deleted){
-    setProperty(SENT_EVENTS, JSON.stringify(sentEvents))
+    let id = event.getId()
+    let end = new Date(event.getEndTime())
+    if(end > now) continue
+    removeFromSentEvents(id)
   }
 }
 
@@ -65,23 +83,18 @@ function deleteFinishedEvents(calendar){
  * @return {bool}
  */
 function isNearEvent(event){
-  let start = new Date(event.getStartTime())
   let now = new Date()
+  let start = new Date(event.getStartTime())
   let limit = minutesLater(now, NOTIFY_BEFORE_MINUTES)
   let inTime = now < start && start < limit
   if(!inTime){
+    removeFromSentEvents(event.getId())
     return false
-  }
-  let sentEvents = JSON.parse(getProperty(SENT_EVENTS))
-  if(!sentEvents){
-    sentEvents = []
-    setProperty(SENT_EVENTS, JSON.stringify(sentEvents))
   }
   let id = event.getId()
   let hasAlreadySent = sentEvents.includes(id)
   if(!hasAlreadySent){
     sentEvents.push(id)
-    setProperty(SENT_EVENTS, JSON.stringify(sentEvents))
   }
   return !hasAlreadySent
 }
